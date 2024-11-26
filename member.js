8d88104cfae421ef3442591ab36afb28ee6c9bdb
st156689 <st156689@stud.uni-stuttgart.de>
2024-11-26 21:48:49 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+    console.log('skillsMember');
+    console.log('this', this);
+    console.log('this.name', this.name);
+    console.log('this.age', this.age);
+    console.log('this.skills', this.skills);
+}
